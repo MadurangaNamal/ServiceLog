7c802f81822a4a9b537c42942d9d0b145e8d3ab7
maduranga <maduranga.wimalarathne95@gmail.com>
2026-03-02 13:10:56 +0400

    Add initial theme and files

---
+++ ServiceLog/Controllers/VehicleServiceController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceLog.Controllers;
+
+public class VehicleServiceController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
